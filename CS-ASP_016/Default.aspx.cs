c71eb486253f4ff9c5d758e26e9ba247a20b6ec9
Robert Graves <robertjgraves@gmail.com>
2015-05-19 09:55:18 -0700

    Initial commit

---
+++ CS-ASP_016/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_016
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void getDateButton_Click(object sender, EventArgs e)
+        {
+            
+            resultLabel.Text = myCalendar.SelectedDate.ToShortDateString();
+        }
+
+        protected void setDateButton_Click(object sender, EventArgs e)
+        {
+            myCalendar.SelectedDate = DateTime.Parse("3/31/2015");
+        }
+
+        protected void showDateButton_Click(object sender, EventArgs e)
+        {
+            DateTime myBirthdate = DateTime.Parse("2/27/1979");
+            myCalendar.VisibleDate = myBirthdate;
+            myCalendar.SelectedDate = myBirthdate;
+        }
+
+        protected void selectedWeekButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "Week of " + myCalendar.SelectedDate.ToShortDateString();
+        }
+
+        protected void myCalendar_SelectionChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = myCalendar.SelectedDate.ToShortDateString();
+        }
+    }
+}
